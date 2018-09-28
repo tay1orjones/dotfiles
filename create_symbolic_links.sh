a9b25f996966893697493a24e1cafb91bd8f27cf
Taylor Jones <taylor.jones826@gmail.com>
2018-09-28 15:48:57 -0500

    fix symbolic links paths and filenames

--- create_symbolic_links.sh
+++ create_symbolic_links.sh
@@ -37,7 +37,7 @@ create_symlinks() {
 
     for i in "${FILES_TO_SYMLINK[@]}"; do
 
-        sourceFile="$(cd .. && pwd)/$i"
+        sourceFile="$(pwd)/$i"
         targetFile="$HOME/.$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
 
         if [ ! -e "$targetFile" ] || $skipQuestions; then
